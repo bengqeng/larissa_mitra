ec384198f62940c159218ede67b773918d632eac
Naufal <muh.naufal@hotmail.com>
2023-01-18 21:44:09 +0700

    'ahahah'

---
+++ app/Models/Mitra.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mitra extends Model
+{
+    use HasFactory;
+
+    // Dont Change No Matter What
+    // ID => NAME
+    public const MITRATIMELINE = [
+        1 => 'FORM',
+        2 => 'a',
+        3 => 'b',
+        4 => 'c',
+        5 => 'd',
+        6 => 'e',
+        7 => 'f',
+        8 => 'g'
+    ];
+}
