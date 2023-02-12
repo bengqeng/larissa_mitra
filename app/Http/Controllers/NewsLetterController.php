ce5096c191df25d0ee1343ed40fca81db9c2a1de
Naufal <muh.naufal@hotmail.com>
2023-02-12 15:19:22 +0700

    add news

--- app/Http/Controllers/NewsLetterController.php
+++ app/Http/Controllers/NewsLetterController.php
@@ -7,12 +7,24 @@ use Illuminate\Http\Request;
 
 class NewsLetterController extends Controller
 {
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $validated = $request->validate([
             'email' => ['email', 'required']
         ]);
-
-        NewsLetter::firstOrCreate(['email' => $validated['email']]);
-        return redirect()->back();
+        $email = NewsLetter::where('email', $request->email)->exists();
+        if ($email) {
+            session(['subscriber' => true]);
+            return redirect()->route('public.index')->with('subscribe', 'Kamu telah subscribe sebelumnya');
+        } else {
+            $result = NewsLetter::firstOrCreate(['email' => $validated['email']]);
+            if ($result) {
+                session(['subscriber' => true]);
+                return redirect()->route('public.index')->with('subscribe', 'Kamu berhasil subscribe, tunggu berita terbaru kami :)');
+            } else {
+                session(['subscriber' => false]);
+                return redirect()->route('public.index')->with('subscribe', 'Kami tidak dapat mendaftarkan email kamu :(');
+            }
+        }
     }
 }
